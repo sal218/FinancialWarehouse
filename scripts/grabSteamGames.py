576219813411a999243c771ef36642fc751db9a4
Callum Brezden <callumbrezden@gmail.com>
2024-02-07 18:37:33 -0800

    Added MySQL Connection

--- scripts/grabSteamGames.py
+++ scripts/grabSteamGames.py
@@ -1,33 +1,69 @@
-import json
 import requests
-import time
+import mysql.connector
 
 
+def connect_to_database():
+    connection = mysql.connector.connect(
+        host="127.0.0.1",  # usually localhost for XAMPP
+        user="root",
+        database="test_steam_db"
+    )
 
+    return connection
 # Get the list of games from the Steam API
-def getSteamGames():
+
+
+games = []
+data_properties = [
+    "name",
+    "required_age",
+    "is_free",
+    "metacritic"
+]
+
+
+def insert_game(connection, game):
+    cursor = connection.cursor()
+
+    # Check if 'metacritic' key exists in game data
+    metacritic_score = game['metacritic']['score'] if 'metacritic' in game and 'score' in game['metacritic'] else None
+
+    insert_query = """
+        INSERT INTO games (name, required_age, is_free, metacritic_score)
+        VALUES (%s, %s, %s, %s)
+    """
+
+    cursor.execute(
+        insert_query, (game['name'], game['required_age'], game['is_free'], metacritic_score))
+
+    connection.commit()
+
+
+def getSteamGames(connection):
     success = 0
     appids = []
     url = "http://api.steampowered.com/ISteamApps/GetAppList/v2"
     response = requests.get(url)
     data = response.json()
+
+    counter = 0
     for app in data['applist']['apps']:
+        if counter >= 250:
+            break
         try:
             appids.append(app['appid'])
-            if getGameDetails(app['appid']):
-                    success += 1
+            if getGameDetails(app['appid'], connection):
+                success += 1
             else:
                 print("Failed: ", app['appid'])
         except:
             pass
-    print("Success: ", success)
+        counter += 1
+    print(f"{success}/{len(appids)}")
     return appids
 
 
-
-
-#Get details of game based on appid
-def getGameDetails(appid):
+def getGameDetails(appid, connection):
     gameDict = {}
     url = "https://store.steampowered.com/api/appdetails?appids=" + str(appid)
     response = requests.get(url)
@@ -35,13 +71,24 @@ def getGameDetails(appid):
 
     if not data or data[str(appid)]['success'] == False:
         return
-    for key in data[str(appid)]['data']:
+
+    for key in data_properties:
         try:
             gameDict[key] = data[str(appid)]['data'][key]
         except:
             pass
-   
-    print(gameDict['name'])
+
+    games.append(gameDict)
+    insert_game(connection, gameDict)
+    print(gameDict)
     return True
 
-x =getSteamGames()
+
+def main():
+    connection = connect_to_database()
+    getSteamGames(connection)
+    connection.close()
+
+
+if __name__ == "__main__":
+    main()
