b71da0e9817d6cadc2387f0c4778f0314525d7b5
Callum Brezden <callumbrezden@gmail.com>
2024-03-10 20:04:09 -0700

    Updated Schema Diagram and Script (#3)

--- scripts/createDatabaseScript.sql
+++ scripts/createDatabaseScript.sql
@@ -10,10 +10,10 @@ CREATE TABLE COMPANY (
 
 CREATE TABLE STOCK (
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    symbol VARCHAR(50),
-    price NUMBER(10,2),
-    volume INT,
     company_id INT,
+    market_cap NUMBER(10, 2),
+    exchange_market VARCHAR(100),
+    yield NUMBER(10, 2),
     FOREIGN KEY (company_id) REFERENCES COMPANY(id)
 );
 
@@ -29,35 +29,57 @@ CREATE TABLE DATE_RECORD (
 CREATE TABLE INDEX_FUND (
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name VARCHAR(255),
-    volume INT
+    management_company VARCHAR(255),
+    net_asset_value NUMBER(10, 2),
+    yield NUMBER(10, 2),
+    type VARCHAR(100)
 );
 
 CREATE TABLE COMMODITY (
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name VARCHAR(255),
-    volume INT
+    unit_of_measure VARCHAR(100),
+    type VARCHAR(100)
 );
 
 CREATE TABLE BOND (
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name VARCHAR(255),
-    amount NUMBER(10, 2)
+    issuer VARCHAR(255),
+    yield NUMBER(10, 2),
+    type VARCHAR(100)
+);
+
+CREATE TABLE CURRENCY (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR(255),
+    country VARCHAR(100),
+    code VARCHAR(10),
+    exhange_rate NUMBER(10, 2)
 );
 
 CREATE TABLE DAILY_TRANSACTIONS (
     stock_id INT,
     date_id INT,
     commodity_id INT,
-    index_fund_id INT,
     bond_id INT,
+    index_fund_id INT,
+    currency_id INT,
     price NUMBER(10, 2),
+    symbol VARCHAR(50),
+    volume INT,
+    open_price NUMBER(10, 2),
+    high_price NUMBER(10, 2),
+    low_price NUMBER(10, 2),
     price_sp500 NUMBER(10, 2),
     price_gold NUMBER(10, 2),
+    price_oil NUMBER(10, 2),
     CONSTRAINT FK_STOCK FOREIGN KEY (stock_id) REFERENCES STOCK(id),
     CONSTRAINT FK_DATE FOREIGN KEY (date_id) REFERENCES DATE_RECORD(id),
     CONSTRAINT FK_COMMODITY FOREIGN KEY (commodity_id) REFERENCES COMMODITY(id),
     CONSTRAINT FK_BOND FOREIGN KEY (bond_id) REFERENCES BOND(id),
-    CONSTRAINT FK_INDEX_FUND FOREIGN KEY (index_fund_id) REFERENCES INDEX_FUND(id)
+    CONSTRAINT FK_INDEX_FUND FOREIGN KEY (index_fund_id) REFERENCES INDEX_FUND(id),
+    CONSTRAINT FK_CURRENCY FOREIGN KEY (currency_id) REFERENCES CURRENCY(id)
 );
 
 SELECT table_name
