4d64fbaffb2fe260fbed88ca32da3aca0a281df2
Callum Brezden <callumbrezden@gmail.com>
2024-03-18 20:28:53 -0700

    Multiple Stock Script working

--- scripts/ETL/stock/stock.py
+++ scripts/ETL/stock/stock.py
@@ -1,3 +1,4 @@
+from itertools import islice
 import os, csv
 from scripts.ETL.utils.stock import Stock_ETL_Util
 
@@ -14,45 +15,56 @@ class Stock_ETL:
 
   def insert_stock_prices(self, csv_file_path, index_csv_file_path):
     with open(index_csv_file_path, 'r') as f:
-      last_row_str = f.read()
-      last_row = int(last_row_str)
+        last_row_str = f.read()
+        last_row = int(last_row_str)
 
+    prev_symbol = None
     with open(csv_file_path, 'r') as file:
         csv_reader = csv.reader(file)
         next(csv_reader)
         transaction_infos = []
-        for i, row in enumerate(csv_reader):
-            if i <= last_row:
-                continue
-            if row[1] != 'MMM':
-                break
+        for i, row in enumerate(islice(csv_reader, last_row, None)):
+            symbol = row[1]
+            company_id = None
+            if symbol != prev_symbol:
+                company_id = self.stock_etl_util.get_company_id_by_symbol(symbol)
+                if company_id is None:
+                    continue
+                self.stock_etl_util.insert_stock(company_id, 0, '', 0)
+                stock_id = self.stock_etl_util.get_stock_id_by_company_id(company_id)
+                prev_symbol = symbol
 
-            transaction_info = {
-                'date': row[0],
-                'price': round(float(row[3]), 2),
-                'symbol': 'MMM',
-                'open_price': round(float(row[6]), 2),
-                'high_price': round(float(row[4]), 2),
-                'low_price': round(float(row[5]), 2),
-                'volume': int(row[7]),
-                'stock_id': 4
-            }
+            try:
+                transaction_info = {
+                    'date': row[0],
+                    'price': round(float(row[3]), 2) if row[3] else 0.0,
+                    'symbol': symbol,
+                    'open_price': round(float(row[6]), 2) if row[6] else 0.0,
+                    'high_price': round(float(row[4]), 2) if row[4] else 0.0,
+                    'low_price': round(float(row[5]), 2) if row[5] else 0.0,
+                    'volume': int(row[7]) if row[7] else 0,
+                    'stock_id': int(stock_id)
+                }
+            except ValueError:
+                continue
 
             transaction_infos.append(transaction_info)
             if (i + 1) % 1000 == 0:
-                self.daily_transactions.insert_daily_transactions(transaction_infos)
+                self.daily_transactions.insert_daily_transactions(
+                    transaction_infos)
                 transaction_infos = []
 
                 # Store the index of the last processed row
-                with open('last_row.txt', 'w') as f:
+                with open(index_csv_file_path, 'w') as f:
                     f.write(str(i))
 
         if transaction_infos:
-            self.daily_transactions.insert_daily_transactions(transaction_infos)
+            self.daily_transactions.insert_daily_transactions(
+                transaction_infos)
 
-            # Store the index of the last processed row
-            with open('last_row.txt', 'w') as f:
-                f.write(str(i))
+        # Store the index of the last processed row
+        with open(index_csv_file_path, 'w') as f:
+            f.write(str(i))
 
   def __del__(self):
     self.script_time_tracker.track_time(self.__class__.__name__)
--- scripts/ETL/utils/stock.py
+++ scripts/ETL/utils/stock.py
@@ -2,9 +2,32 @@ class Stock_ETL_Util:
   def __init__(self, dw_interface):
       self.dw_interface = dw_interface
 
-  # def get_stock_id(self):
-  #     cursor = self.dw_interface.connection.cursor()
-  #     cursor.execute("SELECT MAX(id) FROM stock")
-  #     max_id = cursor.fetchone()[0]
-  #     cursor.close()
-  #     return max_id if max_id is not None else 0
+  def insert_stock(self, company_id, market_cap, exchange_market, yield_value):
+    insert_query = """
+    INSERT INTO STOCK (company_id, market_cap, exchange_market, yield)
+    VALUES (:1, :2, :3, :4)
+    """
+    with self.dw_interface.connection.cursor() as cursor:
+        cursor.execute(insert_query, (company_id, market_cap, exchange_market, yield_value))
+    self.dw_interface.connection.commit()
+    return
+  
+  def get_stock_id_by_company_id(self, company_id):
+    query = "SELECT id FROM STOCK WHERE company_id = :1"
+    with self.dw_interface.connection.cursor() as cursor:
+        cursor.execute(query, (company_id,))
+        result = cursor.fetchone()
+        if result is not None:
+            return result[0]
+        else:
+            return None
+
+  def get_company_id_by_symbol(self, symbol):
+    query = "SELECT id FROM COMPANY WHERE symbol = :1"
+    with self.dw_interface.connection.cursor() as cursor:
+        cursor.execute(query, (symbol,))
+        result = cursor.fetchone()
+        if result is not None:
+            return result[0]
+        else:
+            return None
